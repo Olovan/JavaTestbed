5362f3c1e15fd5618a6c4a01f681eb93829539c6
Olovan <micahsmith.92@gmail.com>
2016-05-16 20:59:10 -0400

    Added Compile script

---
+++ compile.bat
@@ -0,0 +1,2 @@
+javac -cp ".;C:\Programming Libraries\LWJGL\jar\lwjgl.jar;C:\Programming Libraries\LWJGL\native" Main.java
+
--- run.bat
+++ run.bat
@@ -1,1 +1,1 @@
-java -cp "." Main
+java -cp ".;C:\Programming Libraries\LWJGL\jar\lwjgl.jar;C:\Programming Libraries\LWJGL\native" Main
