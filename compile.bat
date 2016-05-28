f8407303573bff9c406c745c1716567da808c1d9
Olovan <micahsmith.92@gmail.com>
2016-05-28 18:01:17 -0400

    Fix batch files for new directory structure

--- compile.bat
+++ compile.bat
@@ -1,2 +1,5 @@
+cd src
 del *.class
-javac -cp ".;C:\Libraries\LWJGL\jar\lwjgl.jar;C:\Libraries\LWJGL\native;C:\Libraries\jglm\target\jglm-1.0.1-SNAPSHOT.jar" Main.java
+javac -cp "../src;C:\Libraries\LWJGL\jar\lwjgl.jar;C:\Libraries\LWJGL\native;C:\Libraries\jglm\target\jglm-1.0.1-SNAPSHOT.jar" Main.java
+move /y ./*.class ../bin
+cd ..
